235ba827658890b8b9b881707f496b3bd408b24b
Chia Yong Kang <46377366+ExtremelySunnyYK@users.noreply.github.com>
2020-12-07 00:49:52 +0800

    added txt file to get words

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+            text.newpen.clear()
+            snake.delay = 0.075
+            text.newpen.color("red")
+            text.newpen.goto(0, 180)
+            text.newpen.penup()
+            text.newpen.hideturtle()
+            text.newpen.write("The Snake is malnourished :( ", align="center", font=("Courier", 15, "normal"))
